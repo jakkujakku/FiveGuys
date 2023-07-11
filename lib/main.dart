88c6be0f8467c6cd264376c4e81b639cd46113b8
jakkujakku <jakkujakku98@gmail.com>
2023-07-11 11:25:32 +0900

    기본 화면 구상

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,182 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(appBar: AppBar(), body: Text("Hello Flutter")),
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 70,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 50.0,
+                  ),
+
+                  // Secret 메뉴 이미지
+                  child: Image.network(
+                    "https://enjoyorangecounty.com/wp-content/uploads/2022/12/five-guys-secret-menu-list-1024x1024.jpg.webp",
+                    height: 400,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style:
+                                ElevatedButton.styleFrom(primary: Colors.white),
+                            child: Image.network(
+                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
+                              height: 100,
+                              width: 60,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8),
+                            child: Container(
+                              child: Text(
+                                "A",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      Column(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style:
+                                ElevatedButton.styleFrom(primary: Colors.white),
+                            child: Image.network(
+                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
+                              height: 100,
+                              width: 60,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8),
+                            child: Container(
+                              child: Text(
+                                "B",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      Column(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style:
+                                ElevatedButton.styleFrom(primary: Colors.white),
+                            child: Image.network(
+                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
+                              height: 100,
+                              width: 60,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8),
+                            child: Container(
+                              child: Text(
+                                "C",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 80, vertical: 16),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style:
+                                ElevatedButton.styleFrom(primary: Colors.white),
+                            child: Image.network(
+                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
+                              height: 100,
+                              width: 60,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8),
+                            child: Container(
+                              child: Text(
+                                "D",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Spacer(
+                        flex: 1,
+                      ),
+                      Column(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style:
+                                ElevatedButton.styleFrom(primary: Colors.white),
+                            child: Image.network(
+                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
+                              height: 100,
+                              width: 60,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8),
+                            child: Container(
+                              child: Text(
+                                "E",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
