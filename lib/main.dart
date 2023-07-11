bf1a3dd6626a599ae739a3a3c751172d3c88d629
jakkujakku <jakkujakku98@gmail.com>
2023-07-11 15:33:07 +0900

    Navigation 반영

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,11 +31,11 @@ class MyApp extends StatelessWidget {
             child: Column(
               children: [
                 const SizedBox(
-                  height: 80,
+                  height: 70,
                 ),
                 Padding(
                   padding: const EdgeInsets.only(
-                    bottom: 40.0,
+                    bottom: 50.0,
                   ),
 
                   // Secret 메뉴 이미지
@@ -34,54 +45,40 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
                   child: Row(
                     children: [
-                      Container(
-                        child: Column(
-                          children: [
-                            // 팀장
-                            ElevatedButton(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                  primary: Colors.white),
-                              child: Stack(
-                                children: [
-                                  Positioned(
-                                    child: Text(
-                                      "(New)",
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w900,
-                                          color: Colors.red),
-                                      textAlign: TextAlign.center,
-                                    ),
-                                    top: 3,
-                                    left: 4,
-                                    right: 5,
-                                  ),
-                                  Image.network(
-                                    "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-sandwiches-icon-01.ashx",
-                                    height: 100,
-                                    width: 60,
-                                  ),
-                                ],
+                      Column(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Profile()),
+                              );
+                            },
+                            style:
+                                ElevatedButton.styleFrom(primary: Colors.white),
+                            child: Image.network(
+                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
+                              height: 100,
+                              width: 60,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8),
+                            child: Container(
+                              child: Text(
+                                "A",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
                               ),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8),
-                              child: Container(
-                                child: Text(
-                                  "의정부부대버거A",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                       Spacer(),
                       Column(
@@ -100,7 +97,7 @@ class MyApp extends StatelessWidget {
                             padding: const EdgeInsets.only(top: 8),
                             child: Container(
                               child: Text(
-                                "의정부부대버거B",
+                                "B",
                                 style: TextStyle(
                                     fontSize: 15,
                                     fontWeight: FontWeight.bold,
@@ -118,7 +115,7 @@ class MyApp extends StatelessWidget {
                             style:
                                 ElevatedButton.styleFrom(primary: Colors.white),
                             child: Image.network(
-                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-dogs-icon-01.ashx",
+                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
                               height: 100,
                               width: 60,
                             ),
@@ -127,7 +124,7 @@ class MyApp extends StatelessWidget {
                             padding: const EdgeInsets.only(top: 8),
                             child: Container(
                               child: Text(
-                                "수원왕갈비",
+                                "C",
                                 style: TextStyle(
                                     fontSize: 15,
                                     fontWeight: FontWeight.bold,
@@ -152,7 +149,7 @@ class MyApp extends StatelessWidget {
                             style:
                                 ElevatedButton.styleFrom(primary: Colors.white),
                             child: Image.network(
-                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-fries-icon-01.ashx",
+                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
                               height: 100,
                               width: 60,
                             ),
@@ -161,7 +158,7 @@ class MyApp extends StatelessWidget {
                             padding: const EdgeInsets.only(top: 8),
                             child: Container(
                               child: Text(
-                                "대구닭강정",
+                                "D",
                                 style: TextStyle(
                                     fontSize: 15,
                                     fontWeight: FontWeight.bold,
@@ -181,7 +178,7 @@ class MyApp extends StatelessWidget {
                             style:
                                 ElevatedButton.styleFrom(primary: Colors.white),
                             child: Image.network(
-                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-drinks-icon-01.ashx",
+                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
                               height: 100,
                               width: 60,
                             ),
@@ -190,7 +187,7 @@ class MyApp extends StatelessWidget {
                             padding: const EdgeInsets.only(top: 8),
                             child: Container(
                               child: Text(
-                                "남양주 닭갈비",
+                                "E",
                                 style: TextStyle(
                                     fontSize: 15,
                                     fontWeight: FontWeight.bold,
@@ -212,90 +209,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("프로필"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: Text("프로필 페이지"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
