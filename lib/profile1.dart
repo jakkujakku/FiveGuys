f3d8fa83d8f6df348dc50aa5003d98181878285d
judas <churoo0418@gmail.com>
2023-07-11 17:39:57 +0900

    철우 프로필 수정본

--- lib/profile1.dart
+++ lib/profile1.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'main.dart';
@@ -30,14 +29,44 @@ class Profile extends StatelessWidget {
       ),
       body: Container(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          child: Container(
-            decoration: new BoxDecoration(
-                borderRadius: new BorderRadius.only(
-                    bottomLeft: const Radius.circular(50.0),
-                    bottomRight: const Radius.circular(50.0)),
-                color: Colors.white),
-          ),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+          child: Stack(children: [
+            Container(
+              decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.only(
+                      bottomLeft: const Radius.circular(50.0),
+                      bottomRight: const Radius.circular(50.0)),
+                  color: Colors.white),
+            ),
+            Row(
+              children: [
+                Container(
+                  alignment: MainAxisAlignment.start,
+                  child: Image.network(
+                    "https://static.vecteezy.com/system/resources/previews/020/486/924/original/business-man-silhouette-icon-human-face-portrait-glyph-pictogram-male-personal-profile-icon-men-user-s-avatar-businessman-manager-office-people-sign-isolated-illustration-vector.jpg",
+                    width: 180,
+                  ),
+                ),
+                Container(
+                    child: Column(
+                  children: [
+                    Text("NAME :"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("MBTI :"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("DATE :"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                )),
+              ],
+            ),
+          ]),
         ),
         color: Colors.red,
       ),
