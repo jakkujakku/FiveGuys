0695a6903aa2d787dc974d0c4b709436f003363b
jakkujakku <jakkujakku98@gmail.com>
2023-07-12 15:16:54 +0900

    프로필 이미지 샘플 작성

--- lib/profile1.dart
+++ lib/profile1.dart
@@ -12,10 +12,13 @@ class Profile extends StatefulWidget {
 
 // List<dynamic> list = List<dynamic>(); // List타입 변수 초기화
 
-Map<String, String> hawaiianBeaches = {
-  'MBTI': 'ENTJ',
-  'Big Island': 'Wailea Bay',
-  'Kauai': 'Hanalei',
+Map<String, String> member = {
+  'Nickname': '대구닭갈비',
+  'Name': 'Kim Jun Woo',
+  'ExpirationDate': '2023.12.31',
+  'Ingredients': 'ENTJ',
+  'Origin': '대구',
+  'ImageUrl': 'https://avatars.githubusercontent.com/u/89556301?v=4'
 };
 
 class _ProfileState extends State<Profile> {
@@ -23,22 +26,32 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: TextButton(
+        // Back버튼
+        leading: BackButton(
+          color: Colors.redAccent,
           onPressed: () {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => FirstPage()),
             );
           },
-          child: Text(
-            " FIVE GUYS ",
-            style: TextStyle(
-                color: Colors.redAccent,
-                fontWeight: FontWeight.w900,
-                fontSize: 30),
-          ),
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.edit),
+              color: Colors.redAccent,
+              onPressed: () {
+                print("Edit 버튼 눌렀다.");
+              })
+        ],
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          " FIVE GUYS ",
+          style: TextStyle(
+              color: Colors.redAccent,
+              fontWeight: FontWeight.w900,
+              fontSize: 30),
         ),
       ),
       body: SingleChildScrollView(
@@ -59,23 +72,30 @@ class _ProfileState extends State<Profile> {
                       Column(
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          Container(
-                            child: Image.network(
-                              "https://static.vecteezy.com/system/resources/previews/020/486/924/original/business-man-silhouette-icon-human-face-portrait-glyph-pictogram-male-personal-profile-icon-men-user-s-avatar-businessman-manager-office-people-sign-isolated-illustration-vector.jpg",
-                              width: 100,
-                              height: 150,
+                          Padding(
+                            padding: EdgeInsets.only(
+                                left: 16.0, right: 10.0, top: 16.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(50.0),
+                              child: Image.network(
+                                "https://avatars.githubusercontent.com/u/89556301?v=4",
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
                         ],
                       ),
                       Container(
                         child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             SizedBox(
                               height: 26,
                             ),
                             Text(
-                              "N A M E : ${hawaiianBeaches['MBTI']}",
+                              "N A M E : ${member['Name']}",
                               style: TextStyle(
                                   fontWeight: FontWeight.w300,
                                   fontSize: 19,
@@ -85,7 +105,7 @@ class _ProfileState extends State<Profile> {
                               height: 10,
                             ),
                             Text(
-                              "M B T I  :",
+                              "M B T I  : ${member['Ingredients']}",
                               style: TextStyle(
                                   fontWeight: FontWeight.w300,
                                   fontSize: 20,
@@ -95,7 +115,7 @@ class _ProfileState extends State<Profile> {
                               height: 10,
                             ),
                             Text(
-                              "D A T E  :",
+                              "D A T E  : ${member['ExpirationDate']}",
                               style: TextStyle(
                                   fontWeight: FontWeight.w300,
                                   fontSize: 19,
