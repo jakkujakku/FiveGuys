4df6ff56d5662174951d01065d5dcbf22d0b1192
t2023-m0075 <t2023-m0075@t2023-m0075ui-MacBookAir.local>
2023-07-12 15:32:43 +0900

    위젯누르면 새창 나오게 수정

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,3 +1,4 @@
+import 'package:fiveguys/main.dart';
 import 'package:fiveguys/profile1.dart';
 import 'package:flutter/material.dart';
 
@@ -6,13 +7,24 @@ class detail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
+    return Scaffold(
+        appBar: AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
       leading: BackButton(
         onPressed: () {
           Navigator.push(
               context, MaterialPageRoute(builder: (context) => Profile()));
         },
+        color: Colors.redAccent,
       ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.delete_outline_outlined),
+          color: Colors.redAccent,
+        ),
+      ],
     ));
   }
 }
--- lib/profile1.dart
+++ lib/profile1.dart
@@ -13,139 +13,147 @@ class Profile extends StatefulWidget {
 
 // List<dynamic> list = List<dynamic>(); // List타입 변수 초기화
 
-Map<String, String> hawaiianBeaches = {
-  'MBTI': 'ENTJ',
-  'Big Island': 'Wailea Bay',
-  'Kauai': 'Hanalei',
+Map<String, String> member = {
+  'Nickname': '대구닭갈비',
+  'Name': 'Kim Jun Woo',
+  'ExpirationDate': '2023.12.31',
+  'Ingredients': 'ENTJ',
+  'Origin': '대구',
+  'ImageUrl': 'https://avatars.githubusercontent.com/u/89556301?v=4'
 };
 
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: BackButton(
+      appBar: AppBar(
+        // Back버튼
+        leading: BackButton(
+          color: Colors.redAccent,
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => FirstPage()),
+            );
+          },
+        ),
+        actions: [
+          IconButton(
             onPressed: () {
               Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => FirstPage()));
+                  MaterialPageRoute(builder: (context) => const detail()));
             },
-            color: Colors.redAccent,
-          ),
-          elevation: 0,
-          backgroundColor: Colors.white,
-          title: Text(
-            "FIVE GUYS",
-            style: const TextStyle(
-                color: Colors.redAccent,
-                fontSize: 30,
-                fontWeight: FontWeight.bold),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const detail()));
-              },
-              icon: Icon(
-                Icons.check_box,
-                color: Colors.redAccent,
-              ),
+            icon: Icon(
+              Icons.edit_outlined,
+              color: Colors.redAccent,
             ),
-          ],
+          ),
+        ],
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          " FIVE GUYS ",
+          style: TextStyle(
+              color: Colors.redAccent,
+              fontWeight: FontWeight.w900,
+              fontSize: 30),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 10, vertical: 200),
-              child: Column(
-                children: [
-                  Stack(children: [
-                    Container(
-                      decoration: new BoxDecoration(
-                          borderRadius:
-                              new BorderRadius.all(Radius.circular(20)),
-                          color: Colors.white),
-                      height: 350,
-                    ),
-                    Row(
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Container(
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+            child: Column(
+              children: [
+                Stack(children: [
+                  Container(
+                    decoration: new BoxDecoration(
+                        borderRadius: new BorderRadius.all(Radius.circular(20)),
+                        color: Colors.white),
+                    height: 150,
+                  ),
+                  Row(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                                left: 16.0, right: 10.0, top: 16.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(50.0),
                               child: Image.network(
-                                "https://static.vecteezy.com/system/resources/previews/020/486/924/original/business-man-silhouette-icon-human-face-portrait-glyph-pictogram-male-personal-profile-icon-men-user-s-avatar-businessman-manager-office-people-sign-isolated-illustration-vector.jpg",
+                                "https://avatars.githubusercontent.com/u/89556301?v=4",
                                 width: 100,
-                                height: 150,
+                                height: 100,
+                                fit: BoxFit.cover,
                               ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 26,
+                            ),
+                            Text(
+                              "N A M E : ${member['Name']}",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 19,
+                                  wordSpacing: 4),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "M B T I  : ${member['Ingredients']}",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 20,
+                                  wordSpacing: 4),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "D A T E  : ${member['ExpirationDate']}",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 19,
+                                  wordSpacing: 4),
+                            ),
+                            SizedBox(
+                              height: 10,
                             ),
                           ],
                         ),
-                        Container(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(
-                                height: 26,
-                              ),
-                              Text(
-                                "N A M E : ${hawaiianBeaches['MBTI']}",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w300,
-                                    fontSize: 19,
-                                    wordSpacing: 4),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                "M B T I  :",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w300,
-                                    fontSize: 20,
-                                    wordSpacing: 4),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                "D A T E  :",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w300,
-                                    fontSize: 19,
-                                    wordSpacing: 4),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Divider(
-                          color: Colors.redAccent,
-                        )
-                      ],
-                    ),
-                  ]),
-                  SizedBox(
-                    height: 30,
+                      ),
+                      Divider(
+                        color: Colors.redAccent,
+                      )
+                    ],
                   ),
-                ],
-              ),
+                ]),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  decoration: new BoxDecoration(
+                      borderRadius: new BorderRadius.all(Radius.circular(20)),
+                      color: Colors.white),
+                  height: 550,
+                ),
+              ],
             ),
-            color: Colors.red,
-            height: 850,
           ),
+          color: Colors.red,
         ),
-        floatingActionButton: FloatingActionButton(
-            elevation: 28,
-            backgroundColor: Color.fromARGB(255, 20, 1, 1),
-            child: Icon(
-              Icons.check_circle_outline_rounded,
-              color: Colors.white,
-            ),
-            onPressed: () {}));
+      ),
+    );
   }
 }
 
@@ -155,12 +163,3 @@ class _ProfileState extends State<Profile> {
 //only(
                   //bottomLeft: const Radius.circular(30.0),
                   //bottomRight: const Radius.circular(30.0)),
-
-
-
-       //           Container(
-         //           decoration: new BoxDecoration(
-           //             borderRadius: new BorderRadius.all(Radius.circular(20)),
-             //           color: Colors.white),
-               //     height: 560,
-                 // ),
