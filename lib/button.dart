94a83b375ba084c2e1454441ce67d59de28d01d7
jakkujakku <jakkujakku98@gmail.com>
2023-07-13 15:11:50 +0900

    정상적으로 올리겠습니다 :)

---
+++ lib/button.dart
@@ -0,0 +1,216 @@
+import 'package:fiveguys/profille.dart';
+import 'package:fiveguys/profille.dart';
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class AButton extends StatelessWidget {
+  const AButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Profile(foodName: "의정부부대버거A")),
+            );
+          },
+          style: ElevatedButton.styleFrom(primary: Colors.white),
+          child: Stack(
+            children: [
+              Positioned(
+                child: Text(
+                  "(New)",
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.red),
+                  textAlign: TextAlign.center,
+                ),
+                top: 8,
+                left: 4,
+                right: 5,
+              ),
+              Image.network(
+                "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-sandwiches-icon-01.ashx",
+                height: 100,
+                width: 60,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Container(
+            child: Text(
+              "의정부부대버거A",
+              style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class BButton extends StatelessWidget {
+  const BButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Profile(foodName: "의정부부대버거B")),
+            );
+          },
+          style: ElevatedButton.styleFrom(primary: Colors.white),
+          child: Image.network(
+            "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
+            height: 100,
+            width: 60,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Container(
+            child: Text(
+              "의정부부대버거B",
+              style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CButton extends StatelessWidget {
+  const CButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Profile(foodName: "대구닭강정")),
+            );
+          },
+          style: ElevatedButton.styleFrom(primary: Colors.white),
+          child: Image.network(
+            "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
+            height: 100,
+            width: 60,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Container(
+            child: Text(
+              "대구따로버거",
+              style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class DButton extends StatelessWidget {
+  const DButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Profile(foodName: "남양주닭갈비버거")),
+            );
+          },
+          style: ElevatedButton.styleFrom(primary: Colors.white),
+          child: Image.network(
+            "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
+            height: 100,
+            width: 60,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Container(
+            child: Text(
+              "남양주닭갈비버거",
+              style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class EButton extends StatelessWidget {
+  const EButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Profile(foodName: "수원갈비버거")),
+            );
+          },
+          style: ElevatedButton.styleFrom(primary: Colors.white),
+          child: Image.network(
+            "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
+            height: 100,
+            width: 60,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Container(
+            child: Text(
+              "수원갈비버거",
+              style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:fiveguys/button.dart';
+import 'package:fiveguys/editPage.dart';
 import 'package:flutter/material.dart';
+import 'editPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -43,375 +46,35 @@ class FirstPage extends StatelessWidget {
                     fit: BoxFit.fill,
                   ),
                 ),
-                // 의정부부대버거A 버튼
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 24),
                   child: Row(
                     children: [
-                      Column(
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        Profile(foodName: "의정부부대버거A")),
-                              );
-                            },
-                            style:
-                                ElevatedButton.styleFrom(primary: Colors.white),
-                            child: Image.network(
-                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-sandwiches-icon-01.ashx",
-                              height: 100,
-                              width: 60,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8),
-                            child: Container(
-                              child: Text(
-                                "의정부부대버거A",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                      AButton(),
                       Spacer(),
-                      Column(
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        Profile(foodName: "의정부부대버거B")),
-                              );
-                            },
-                            style:
-                                ElevatedButton.styleFrom(primary: Colors.white),
-                            child: Image.network(
-                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-burgers-icon-01.ashx",
-                              height: 100,
-                              width: 60,
-                            ),
-                          ),
-                          // 의정부부대버거B 버튼
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8),
-                            child: Container(
-                              child: Text(
-                                "의정부부대버거B",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      // ...
+                      BButton(),
+                      Spacer(),
+                      CButton(),
                     ],
                   ),
                 ),
-                // 대구 닭강정 버튼
                 Padding(
                   padding:
                       const EdgeInsets.symmetric(horizontal: 80, vertical: 16),
                   child: Row(
                     children: [
-                      Column(
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        Profile(foodName: "대구닭강정")),
-                              );
-                            },
-                            style:
-                                ElevatedButton.styleFrom(primary: Colors.white),
-                            child: Image.network(
-                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-fries-icon-01.ashx",
-                              height: 100,
-                              width: 60,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8),
-                            child: Container(
-                              child: Text(
-                                "대구닭강정",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
                       Spacer(
                         flex: 1,
                       ),
-                      Column(
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        Profile(foodName: "남양주 닭갈비")),
-                              );
-                            },
-                            style:
-                                ElevatedButton.styleFrom(primary: Colors.white),
-                            child: Image.network(
-                              "https://www.fiveguys.co.kr/-/media/public-site/images/menu-images/menu-category-icons/fg-drinks-icon-01.ashx",
-                              height: 100,
-                              width: 60,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8),
-                            child: Container(
-                              child: Text(
-                                "남양주 닭갈비",
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                      EButton(),
+                      Spacer(),
+                      DButton()
                     ],
                   ),
                 )
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class Profile extends StatefulWidget {
-  final String foodName;
-
-  const Profile({Key? key, required this.foodName}) : super(key: key);
-
-  @override
-  _ProfileState createState() => _ProfileState();
-}
-
-class _ProfileState extends State<Profile> {
-  Map<String, String> foodDetails = {};
-
-  @override
-  void initState() {
-    super.initState();
-
-    switch (widget.foodName) {
-      case "의정부부대버거A":
-        foodDetails = {
-          'Name': '버거A',
-          'Date': '2023.12.31',
-          'Taste': '매운맛',
-          'Origin': '의정부',
-        };
-        break;
-
-      case "의정부부대버거B":
-        foodDetails = {
-          'Name': '버거B',
-          'Date': '2023.11.30',
-          'Taste': '담백한맛',
-          'Origin': '서울',
-        };
-        break;
-
-      case "대구닭강정":
-        foodDetails = {
-          'Name': '닭강정',
-          'Date': '2023.10.30',
-          'Taste': '달달한맛',
-          'Origin': '대구',
-        };
-        break;
-
-      case "남양주 닭갈비":
-        foodDetails = {
-          'Name': '닭갈비',
-          'Date': '2023.09.30',
-          'Taste': '향긋한맛',
-          'Origin': '남양주',
-        };
-        break;
-
-      default:
-        foodDetails = {
-          'Name': '기본음식',
-          'Date': '2023.01.01',
-          'Taste': '기본맛',
-          'Origin': '기본',
-        };
-        break;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: BackButton(
-          color: Colors.redAccent,
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.edit),
-            color: Colors.redAccent,
-            onPressed: () {
-              print("Edit 버튼 눌렀다.");
-            },
-          ),
-        ],
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: Text(
-          " FIVE GUYS ",
-          style: TextStyle(
-            color: Colors.redAccent,
-            fontWeight: FontWeight.w900,
-            fontSize: 30,
-          ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      decoration: new BoxDecoration(
-                        borderRadius: new BorderRadius.all(
-                          Radius.circular(20),
-                        ),
-                        color: Colors.white,
-                      ),
-                      height: 150,
-                    ),
-                    Row(
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(
-                                left: 16.0,
-                                right: 10.0,
-                                top: 16.0,
-                              ),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(50.0),
-                                child: Image.network(
-                                  "https://avatars.githubusercontent.com/u/89556301?v=4",
-                                  width: 100,
-                                  height: 100,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Divider(
-                      color: Colors.redAccent,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  decoration: new BoxDecoration(
-                    borderRadius: new BorderRadius.all(
-                      Radius.circular(20),
-                    ),
-                    color: Colors.white,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 26,
-                      ),
-                      Text(
-                        "이름 : ${foodDetails['Name']}",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 19,
-                            wordSpacing: 4),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        "성분 : ${foodDetails['Taste']}",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 20,
-                            wordSpacing: 4),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        "유통기한 : ${foodDetails['Date']}",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 19,
-                            wordSpacing: 4),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        "원산지 : ${foodDetails['Origin']}",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 19,
-                            wordSpacing: 4),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                    ],
-                  ),
-                  height: 550,
-                ),
-              ],
-            ),
-          ),
-          color: Colors.red,
         ),
       ),
     );
